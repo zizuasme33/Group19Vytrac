83c270998f85f5637bbbd27383c922857975fe23
tolga <t.yigir35@gmail.com>
2021-05-25 19:39:39 -0400

    1

--- src/test/java/VyTrack.java
+++ src/test/java/VyTrack.java
@@ -10,14 +10,16 @@ public class VyTrack {
 
     public static void main(String[] args) throws InterruptedException {
         TruckDriver("User154", "UserUser123");
-
+        /**Please Create Your role Method and push the code.
+         Don't scare to play with git up. I'm have back up :))
+         */
     }
 
     public static void TruckDriver(String username1, String password1) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         // 1. Open Chrome browser
         WebDriver driver = new ChromeDriver();
-
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         //2. Given I am on login page : https://qa3.vytrack.com/user/login
@@ -32,7 +34,6 @@ public class VyTrack {
         WebElement password = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
         password.click();
         password.sendKeys(password1);
-
         //6. And Click Login button
         WebElement loginButton = driver.findElement(By.xpath("//button[@id='_submit']"));
         loginButton.click();
